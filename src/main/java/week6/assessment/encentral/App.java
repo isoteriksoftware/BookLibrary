1b337ac665145cd5a340de11bec40c2fb4be9853
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-09-12 09:46:34 +0100

    Initial commit

---
+++ src/main/java/week6/assessment/encentral/App.java
@@ -0,0 +1,13 @@
+package week6.assessment.encentral;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
